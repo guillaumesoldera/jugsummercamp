91f8ab86947a191055c9df99a4f87acab636df65
fedysalah <fedy.salah@gmail.com>
2018-09-11 10:18:56 +0200

    fix some styles

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {Header} from '../components';
 import '../styles/NoMatch.css'
 
 export class NoMatch extends Component {
     render() {
         return (
+            <Fragment>
+            <Header />
+                <div className="content">
             <div className="no-match-container">
                 <h4>Ouh là y a rien ici !</h4>
             </div>
+                </div>
+            </Fragment>
+
         );
     }
 }
