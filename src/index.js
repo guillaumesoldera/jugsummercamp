5195496f3977f6c1e8100a344925696d6b374803
fedysalah <fedy.salah@serli.com>
2018-09-12 23:57:21 +0200

    step 0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './styles/index.css';
 import './styles/materialize.css';
 import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
