921f6174ab87613239257208f83193c5954e971f
fedysalah <fedy.salah@serli.com>
2018-09-14 17:05:50 +0200

    update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -32,8 +32,45 @@ export default function register() {
           };
         };
         //register stuff
+        navigator.serviceWorker.ready.then(() => {
+            registerForNotifications(registration);
+        })
       })
       .catch(error => {
         console.error('Error during service worker registration:', error);
       });
+  }
+
+
+  function registerForNotifications(registration){
+    console.log("Registering Push...");
+    registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array("BFwbGBPX9ggNKmMPMtn8a_eYfMaU28iGv8-fy8PwxoMPwZZQQKaq96RMTCBkdUvVDjgJPZ6wtBeZ2p2i09ZMihY")
+    }).then((subscription) => {
+        console.log("Push Registered...");
+        console.log("Sending Push...");
+        fetch("/api/subscribe", {
+            method: "POST",
+            body: JSON.stringify(subscription),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(() => {
+            console.log("Push Sent...");
+        });
+    }).catch(error => {
+        console.error('Error during pushManager subscription:', error);
+    });
+  }
+  
+  function urlBase64ToUint8Array(base64String) {
+      const padding = "=".repeat((4 - base64String.length % 4) % 4);
+      const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+      for (let i = 0; i < rawData.length; ++i) {
+          outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
   }
